1aa1b8c2e1481caa74950bf9867a120311bbbdde
Brad Caron <bacaron@iu.edu>
2020-03-24 21:18:47 -0400

    Update fsl-anat.sh

--- fsl-anat.sh
+++ fsl-anat.sh
@@ -71,6 +71,8 @@ sed -i "/--refmask=/s/$/${TEMPLATE}_mask_dil1/" ./fnirt_config.cnf
 
 ## run fsl_anat
 echo "running fsl_anat"
+[ -d ${tempdir}.anat ] && rm ${tempdir}.anat
+[ -f ${biasdir}/t1.nii.gz ] && rm ${biasdir}/t1.nii.gz && rm ./*.nii.gz
 [ ! -f ${tempdir}.anat/${input_type}_biascorr.nii.gz ] && fsl_anat -i ${input} \
 	--noreorient \
 	--nocrop \
